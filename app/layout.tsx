85f4a964fa7b5d23a882a102ff67134c7c44f5e9
hilman <healmanunsorry0000@gmail.com>
2025-07-16 18:56:55 +0700

    feat

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
         <html lang="en">
             <body className="font-poppins">
                 <AuthProvider>
-                    <Navbar />
+                    
                     <main className="container mx-auto ">{children}</main>
                 </AuthProvider>
             </body>
